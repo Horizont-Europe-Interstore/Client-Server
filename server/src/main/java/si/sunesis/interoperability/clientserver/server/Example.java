6519199ba6f4551329fce6ac84cc72dc6e0a6515
David Trafela <dt1193@student.uni-lj.si>
2025-03-07 12:50:41 +0100

    Common module deleted, now using interoperability-common

--- server/src/main/java/si/sunesis/interoperability/clientserver/server/Example.java
+++ server/src/main/java/si/sunesis/interoperability/clientserver/server/Example.java
@@ -1,13 +1,37 @@
+/*
+ *  Copyright (c) 2023-2024 Sunesis and/or its affiliates
+ *  and other contributors as indicated by the @author tags and
+ *  the contributor list.
+ *
+ *  Licensed under the MIT License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  https://opensource.org/licenses/MIT
+ *
+ *  The software is provided "AS IS", WITHOUT WARRANTY OF ANY KIND, express or
+ *  implied, including but not limited to the warranties of merchantability,
+ *  fitness for a particular purpose and noninfringement. in no event shall the
+ *  authors or copyright holders be liable for any claim, damages or other
+ *  liability, whether in an action of contract, tort or otherwise, arising from,
+ *  out of or in connection with the software or the use or other dealings in the
+ *  software. See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package si.sunesis.interoperability.clientserver.server;
 
+import ieee.std._2030_5.ns.Event;
+import io.nats.client.Connection;
 import io.nats.client.Options;
 import lombok.extern.slf4j.Slf4j;
-import org.ieee._2030.Event;
-import si.sunesis.interoperability.clientserver.common.IEEEObjectFactory;
+import si.sunesis.interoperability.common.exceptions.HandlerException;
+import si.sunesis.interoperability.common.ieee2030dot5.IEEEObjectFactory;
 import si.sunesis.interoperability.nats.NatsConnection;
 
-import java.io.IOException;
-
+/**
+ * @author David Trafela, Sunesis
+ * @since 1.0.0
+ */
 @Slf4j
 public class Example {
 
@@ -15,9 +39,7 @@ public class Example {
         Options options = new Options.Builder().server("nats://localhost:4222").build();
 
         NatsConnection natsConnection = new NatsConnection();
-        try {
-            natsConnection.connectSync(options, false);
-
+        try (Connection ignored = natsConnection.connectSync(options, false)) {
             Server server = new Server(natsConnection);
 
             server.subscribe("devices.event", message -> {
@@ -27,9 +49,13 @@ public class Example {
                 Event event = IEEEObjectFactory.fromXMLToIEEE(msg, Event.class);
                 // Do something with the event
 
-                server.publish("events.event", IEEEObjectFactory.fromIEEEToJSON(event));
+                try {
+                    server.publish("events.event", IEEEObjectFactory.fromIEEEToJSON(event));
+                } catch (HandlerException e) {
+                    log.error("Failed to publish event", e);
+                }
             });
-        } catch (IOException | InterruptedException e) {
+        } catch (Exception e) {
             log.error("Failed to connect to NATS server", e);
         }
     }
